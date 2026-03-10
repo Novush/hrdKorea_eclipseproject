2db0850863b1ae64ae4d3c02d4a3a5a5bf97c983
Novush <novus0917@gmail.com>
2026-03-10 10:37:37 +0900

    [Fix] (sql.sql) - umber를 number로 오타 해결
    create table money_tbl_02(
    custno number(6) not null,
    salenol number(8) not null,
    pcost number(8),
    amount number(4),
    price number(8),
    pcode varchar2(4),
    sdate date,
    primary key(custno,salenol)
    );

--- .metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/hrd_30213/sql.sql
+++ .metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/hrd_30213/sql.sql
@@ -23,7 +23,7 @@ select * from member_tbl_02;
 
 
 create table money_tbl_02(
-custno umber(6) not null,
+custno number(6) not null,
 salenol number(8) not null,
 pcost number(8),
 amount number(4),
